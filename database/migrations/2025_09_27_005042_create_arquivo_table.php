038468966f9fdd373abe684961fe0e42d2d3400e
esteves014 <vitormiguel1195@gmail.com>
2025-09-26 23:07:46 -0300

    commit

---
+++ database/migrations/2025_09_27_005042_create_arquivo_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('arquivo', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('nome');
+            $table->string('caminho');
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('arquivo');
+    }
+};
---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,11 @@
+@extends('site.layout')
+@section('titulo', 'Inicio-Cloudrive')
+@section('conteudo')
+
+    <main class="container-fluid mt-5 bg-white" id="main-home">
+        <section class="container">
+            <h1>Olá! Bem vindo ao Cloudrive</h1>
+            <p></p>
+        </section>
+    </main>
+@endsection
